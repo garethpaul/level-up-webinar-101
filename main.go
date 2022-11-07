0625445c9190c09ee863d8d9419c38b14555ccbe
Gareth Paul Jones (GPJ) <gareth@garethpaul.com>
2022-11-07 13:37:59 -0800

    Add main.go

---
+++ main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	twilio "github.com/twilio/twilio-go"
+	openapi "github.com/twilio/twilio-go/rest/api/v2010"
+)
+
+func main() {
+    client := twilio.NewRestClient()
+
+    params := &openapi.CreateMessageParams{}
+    params.SetTo(os.Getenv("TO_PHONE_NUMBER"))
+    params.SetFrom(os.Getenv("TWILIO_PHONE_NUMBER"))
+    params.SetBody("Hello from Golang!")
+
+    _, err := client.Api.CreateMessage(params)
+    if err != nil {
+        fmt.Println(err.Error())
+    } else {
+        fmt.Println("SMS sent successfully!")
+    }
+}
